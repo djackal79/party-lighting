e3550101a65841f2c236c43739c80d3f873d735b
Carl Heinrich <carl,heinrich@gmail.com>
2026-08-09 15:17:12 +1000

    Configure Astro Cloudflare adapter to bypass Miniflare prerendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://{{LIGHTING_DOMAIN}}',
   integrations: [sitemap()],
+  adapter: cloudflare({
+    prerenderEnvironment: 'node'
+  })
 });
